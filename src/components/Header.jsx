d16815b8eeb83622bf40b9b6f669c7fefb029c05
Bekzod <bekzodisakov18@gmail.com>
2023-10-19 11:29:56 +0500

    fetch with useQuery

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,54 @@
 import React, { useEffect, useRef, useState } from "react";
-import { getCurrentDate, getCurrentLocation } from "@utilities";
+import { getCurrentDate } from "lib/utilities";
 import { Spinner, SpriteIcon } from "@reusable-components";
+import { useQuery } from "@tanstack/react-query";
+import useDebounce from "lib/hooks/useDebounce";
+
+function useSearch(search) {
+  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${search}`;
+  return useQuery({
+    queryKey: ["search", search],
+    queryFn: async () => {
+      if (!search || search.length === 1) {
+        return [];
+      }
+
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error("Network repsonse was not ok");
+      }
+
+      const data = await response.json();
+      return data.results;
+    },
+  });
+}
 
 export const Header = () => {
+  const [locationName, setLocationName] = useState("");
+
+  const search = useDebounce(locationName, 300);
+  const { isFetching, isError, data, error } = useSearch(search);
+
   const checkboxRef = useRef(null);
   const searchInputRef = useRef(null);
-  const [searchKey, setSearchKey] = useState("");
-  const [data, setData] = useState(null);
   const [selectedLocation, setSelectedLocation] = useState(null);
   const [defaultLocation, setDefaultLocation] = useState(null);
-
-  console.log({ selectedLocation });
-  console.log({ defaultLocation });
 
   const date = getCurrentDate(
     selectedLocation?.timezone || defaultLocation?.time_zone.id
   );
 
-  let result = null;
+  let content = null;
 
-  if (data === "loading") {
-    result = <Spinner />;
-  } else if (data?.length) {
-    console.log(data);
-    result = (
+  if (isFetching) {
+    content = <Spinner />;
+  } else if (isError) {
+    console.error(error);
+    content = <div className='px-2 text-red-500'>{error.message}</div>;
+  } else if (data) {
+    content = (
       <>
         {data.map((location) => (
           <li key={location.id} className='my-2 px-2'>
@@ -31,8 +56,7 @@ export const Header = () => {
               className='block text-left w-full'
               onClick={() => {
                 setSelectedLocation(location);
-                setData(null);
-                setSearchKey("");
+                setLocationName("");
               }}
             >
               <div className='text-custom-off-white text-sm'>
@@ -46,46 +70,15 @@ export const Header = () => {
         ))}
       </>
     );
-  } else if (data === undefined) {
-    result = <div className='p-1 text-center'>Not found</div>;
   }
 
   useEffect(() => {
-    if (!searchKey) {
-      setData(null);
-      return;
-    }
-    let ignore = false;
-
-    async function searchData() {
-      try {
-        const response = await fetch(
-          `https://geocoding-api.open-meteo.com/v1/search?name=${searchKey}`
-        );
-        const data = await response.json();
-        if (ignore) return;
-        setData(data.results);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-
-    const timeOut = setTimeout(() => {
-      searchData();
-      setData("loading");
-    }, 500);
-
-    return () => {
-      ignore = true;
-      clearTimeout(timeOut);
-    };
-  }, [searchKey]);
-
-  useEffect(() => {
     const detectCurrentLocation = async () => {
-      const location = await getCurrentLocation();
+      const res = await fetch(
+        `https://api.ipregistry.co/?key=${process.env.REACT_APP_IP_REGISTRY_KEY}`
+      );
+      const location = await res.json();
       if (location) {
-        console.log(location);
         setDefaultLocation(location);
       }
     };
@@ -95,14 +88,14 @@ export const Header = () => {
 
   useEffect(() => {
     function handleClickOutside(e) {
-      if (!e.target.closest("#search-input-wrapper") && !searchKey) {
+      if (!e.target.closest("#search-input-wrapper") && !locationName) {
         checkboxRef.current.checked = false;
       }
     }
 
     window.addEventListener("click", handleClickOutside);
     return () => window.removeEventListener("click", handleClickOutside);
-  }, [searchKey]);
+  }, [locationName]);
 
   return (
     <header className='flex items-start justify-between mb-4'>
@@ -125,8 +118,8 @@ export const Header = () => {
           ref={checkboxRef}
         />
         <input
-          value={searchKey}
-          onChange={(e) => setSearchKey(e.target.value)}
+          value={locationName}
+          onChange={(e) => setLocationName(e.target.value)}
           type='text'
           placeholder='Enter city'
           className='bg-custom-bg-outer peer-checked:px-1 peer-checked:mr-2 focus:mr-2 rounded-sm w-0 peer-checked:w-40 focus:w-40 duration-100'
@@ -136,7 +129,7 @@ export const Header = () => {
           <SpriteIcon name='search-icon' width='15' height='15' />
         </label>
         <ul className='absolute z-10 w-full rounded-md left-0 top-[100%] border-t-2 border-t-custom-bg-inner bg-custom-bg-outer/30 backdrop-blur-sm shadow-lg'>
-          {result}
+          {content}
         </ul>
       </div>
     </header>
