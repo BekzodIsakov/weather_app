090ebf7153d68e46eabe6358e30419be0651590b
Bekzod <bekzodisakov18@gmail.com>
2023-10-20 19:41:05 +0500

    Delete AppInnerWrapper component

--- src/components/AppInnerWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const AppInnerWrapper = ({ children }) => {
-  return (
-    <div className='flex flex-col max-w-md m-4 rounded-3xl grow bg-custom-bg-inner text-zinc-50 px-4 pt-8 pb-5 min-h-[84vh] max-h-[90vh] overflow-hidden'>
-      {children}
-    </div>
-  );
-};
