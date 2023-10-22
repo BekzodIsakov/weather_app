8262ca4875609775cfb1cb1a800cfb4a6ae3f961
Bekzod <bekzodisakov18@gmail.com>
2023-10-22 11:17:31 +0500

    remove extra imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { useState } from "react";
-import {
-  Header,
-  MaxTemperature,
-  WeatherTabs,
-  CurrentWeather,
-} from "@components";
+import { Header, WeatherTabs, CurrentWeather } from "@components";
 
 function App() {
   const [location, setLocation] = useState(null);
@@ -13,9 +8,9 @@ function App() {
     <div className='flex justify-center items-center min-h-screen bg-custom-bg-outer text-custom-off-white font-chakra'>
       <div className='flex flex-col max-w-md m-4 rounded-3xl grow bg-custom-bg-inner text-zinc-50 px-4 pt-8 pb-5 min-h-[84vh] max-h-[90vh] overflow-hidden'>
         <Header location={location} setLocation={setLocation} />
-        {/* <MaxTemperature /> */}
         <CurrentWeather location={location} />
-        <WeatherTabs />
+        {/* <MaxTemperature /> */}
+        {/* <WeatherTabs /> */}
       </div>
     </div>
   );
